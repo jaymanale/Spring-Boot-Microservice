f60f9d4e63cf527d5b507b837dd348bf75a677f9
jaymanale <jaymanale29@gmail.com>
2023-11-30 08:42:44 +0530

    first commit

---
+++ src/main/java/com/spring/microservice/springbootmicroservicerestapi/SpringbootMicroserviceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservice.springbootmicroservicerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceRestApiApplication.class, args);
+	}
+
+}
