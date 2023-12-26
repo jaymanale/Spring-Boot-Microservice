a2b7638c32b3e87efe2f3e2847b00ff9134367d2
jaymanale <jaymanale29@gmail.com>
2023-12-26 20:45:05 +0530

    Path Variable end point add

--- src/main/java/com/spring/microservice/springbootmicroservicerestapi/controller/StudentController.java
+++ src/main/java/com/spring/microservice/springbootmicroservicerestapi/controller/StudentController.java
@@ -4,18 +4,19 @@ import com.spring.microservice.springbootmicroservicerestapi.bean.Student;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-
+  // Get a Student data
   //http://localhost:8080/student
 @GetMapping("student")
   public Student getStudent(){
     Student student = new Student(1,"jay","manale");
     return student;
   }
-
+  // Get List Of Students
   //http://localhost:8080/students
   @GetMapping("students")
   public List<Student> getStudentList() {
@@ -26,4 +27,13 @@ public class StudentController {
 
     return studentList;
   }
+
+  // Get Student information based on PathVariable info
+  //http://localhost:8080/students/1
+
+  @GetMapping("students/{id}/{first-name}/{last-name}")
+  public Student getPathVariableStudent(@PathVariable int id, @PathVariable("first-name") String firstName, @PathVariable("last-name") String lastName){
+  return new Student(id,firstName,lastName);
+  }
+
 }
