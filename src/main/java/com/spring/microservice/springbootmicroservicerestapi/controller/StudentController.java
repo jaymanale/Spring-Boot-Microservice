f096a629c34814b278d5261ff2e492b21c6136df
jaymanale <jaymanale29@gmail.com>
2023-12-28 18:27:47 +0530

    Delete endpoint to delete resource

--- src/main/java/com/spring/microservice/springbootmicroservicerestapi/controller/StudentController.java
+++ src/main/java/com/spring/microservice/springbootmicroservicerestapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.spring.microservice.springbootmicroservicerestapi.bean.Student;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,6 +73,14 @@ public class StudentController {
     System.out.println(student.getId());
     System.out.println(student.getFirstName());
     return student;
+  }
+
+  // Spring Boot REST API  -  Delete -> Delete existing resource
+  //http://localhost:8080/students/{id}/delete
+
+  @DeleteMapping("students/{id}/delete")
+  public String deleteStudent(@PathVariable("id") int studentId){
+  return "Student with ID : "+studentId + " deleted successfully";
   }
 
 }
