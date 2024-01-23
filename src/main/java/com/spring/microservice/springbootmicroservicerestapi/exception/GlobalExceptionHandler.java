f8e91a2f20aea0277281aba00714a1f575d3a47a
jaymanale <jaymanale29@gmail.com>
2024-01-23 10:33:53 +0530

    handle Global Exception add

--- src/main/java/com/spring/microservice/springbootmicroservicerestapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/microservice/springbootmicroservicerestapi/exception/GlobalExceptionHandler.java
@@ -9,8 +9,21 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+  // Handle Global Exception
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorDetails> handleResourceNotFoundException(Exception exception, WebRequest webRequest){
 
+    ErrorDetails errorDetails  = new ErrorDetails(
+        LocalDateTime.now(),
+        exception.getMessage(),
+        webRequest.getDescription(false),
+        "INTERNAL_SERVER_ERROR"
+    );
 
+    return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  // Handle Custom Exception
   @ExceptionHandler(ResourceNotFoundException.class)
   public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException, WebRequest webRequest){
 
